2edfa5cd1cddba258e909492d5d0eae02ec282be
Rahul Mahajan <aerorahul@users.noreply.github.com>
2016-05-12 21:00:55 +0000

    print now prints on the same line
    git-svn-id: https://svnemc.ncep.noaa.gov/projects/sandbox/Rahul.Mahajan/trunk/python_lib@75476 281fb692-3a18-0410-826d-9e847983f365

--- lib_utils.py
+++ lib_utils.py
@@ -9,7 +9,7 @@
 ###############################################################
 
 '''
-lib_utils.py contains commonly used utility functions
+lib_utils.py contains handy utility functions
 '''
 
 __author__ = "Rahul Mahajan"
@@ -17,11 +17,17 @@ __email__ = "rahul.mahajan@nasa.gov"
 __copyright__ = "Copyright 2016, NOAA / NCEP / EMC"
 __license__ = "GPL"
 __status__ = "Prototype"
-__all__ = ['float10Power','roundNumber']
+__all__ = [
+            'float10Power','roundNumber',
+            'pickle','unpickle',
+            'writeHDF','readHDF',
+            'EmptyDataFrame'
+          ]
 
 
 import numpy as _np
-
+import cPickle as _pickle
+import pandas as _pd
 
 def float10Power(value):
     if value == 0:
@@ -35,8 +41,84 @@ def float10Power(value):
 
 
 def roundNumber(value):
+    '''
+    Input: Number
+    Output: Number rounded to the nearest 10th.
+    eg.
+    0.01231 => 0.01    0.0164  => 0.02
+    2.3     => 2.0     2.8     => 3.0
+    6.2     => 10
+    59      => 60
+    '''
 
     d = float10Power(value)
     round_value = _np.round(abs(value)/10**d) * 10**d * _np.sign(value)
 
     return round_value
+
+
+def pickle(fname,data):
+    '''
+    fname - filename to pickle to
+    data  - data to pickle
+    '''
+    print('pickling ... %s' % fname)
+    try:
+        _pickle.dump(data, open(fname, "wb"))
+    except _pickle.PicklingError:
+        raise
+    print ' ... done'
+    return
+
+
+def unpickle(fname):
+    '''
+    fname - filename to unpickle to
+    '''
+    print('unpickling ... %s' % fname),
+    try:
+        data = _pickle.load(open(fname, "rb"))
+    except _pickle.UnpicklingError:
+        raise
+    print ' ... done'
+    return data
+
+
+def writeHDF(fname,vname,data):
+    print('writing ... %s' % fname),
+    try:
+        hdf = _pd.HDFStore(fname)
+        hdf.put(vname,data,format='table',append=True)
+        hdf.close()
+    except RuntimeError:
+        raise
+    print ' ... done'
+    return
+
+
+def readHDF(fname,vname,**kwargs):
+    print('reading ... %s' % fname),
+    try:
+        data = _pd.read_hdf(fname,vname,**kwargs)
+    except RuntimeError:
+        raise
+    print ' ... done'
+    return data
+
+
+def EmptyDataFrame(columns,names,dtype=None):
+    '''
+        Create an empty Multi-index DataFrame
+        Input:
+            columns = 'name of all columns; including indices'
+            names = 'name of index columns'
+        Output:
+            df = Multi-index DataFrame object
+    '''
+
+    levels = [[] for i in range(len(names))]
+    labels = [[] for i in range(len(names))]
+    indices = _pd.MultiIndex(levels=levels,labels=labels,names=names)
+    df = _pd.DataFrame(index=indices, columns=columns, dtype=dtype)
+
+    return df
